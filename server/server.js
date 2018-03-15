e9f44faba17748b1337a245edb4b352019bad2b6
thejenbo <jbormacoff@rehabs.com>
2018-03-15 11:57:09 -0700

    ADD heroku scripts, REMOVE build files

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -10,6 +11,6 @@ app.get('*', (req, res) => {
 	res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(3000, () => {
-
+app.listen(port, () => {
+	console.log('server is running');
 });
