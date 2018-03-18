1488aa414b4da7159942a9d6eee4e8375fd22abe
thejenbo <jbormacoff@gmail.com>
2018-03-18 11:31:36 -0700

    ADD moment and numeral formatting to expenseListItem

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({ description, amount, createdAt, id }) => (
     <div>
         <h3>{description}</h3>
-        <p>{amount}, {createdAt}</p>
+        <p>amount: {numeral(amount / 100).format('$0,0.00')}</p>
+        <p>date: {moment(createdAt).format('MMMM Do, YYYY')}</p>
         <Link to={`/edit/${id}`}>
             edit
         </Link>
