2c84c04aaed787f883050f20940bdac86dea936e
thejenbo <jbormacoff@gmail.com>
2018-03-27 08:15:01 -0700

    ADD remove and update expenses in firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -35,17 +35,26 @@ export const removeExpense = ({id} = {}) => ({
 export const startRemoveExpense = ({id} = {}) => {
     return (dispatch) => {
         return database.ref(`expenses/${id}`).remove()
-        .then(() => {
-            dispatch(removeExpense({id}));
-        });
-    }
-}
+            .then(() => {
+                dispatch(removeExpense({id}));
+            });
+    };
+};
 
 export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
     id,
     updates
 });
+
+export const startEditExpense = (id, updates) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).update(updates)
+            .then(() => {
+                dispatch(editExpense(id, updates));
+            });
+    };
+};
 
 export const setExpenses = (expenses) => ({
     type: 'SET_EXPENSES',
@@ -68,5 +77,5 @@ export const startSetExpenses = () => {
 
                 dispatch(setExpenses(expenses));
             });
-    }
-}
+    };
+};
